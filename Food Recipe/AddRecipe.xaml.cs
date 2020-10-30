9ffe1a8b07f8ae93da7b7856e3d8e0cd337dfd08
THẮNG VÕ ĐỨC <18120557@student.hcmus.edu.vn>
2020-10-30 11:50:29 +0700

    demo AddRecipe

---
+++ Food Recipe/AddRecipe.xaml.cs
@@ -0,0 +1,84 @@
+﻿using MaterialDesignThemes.Wpf;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+namespace Food_Recipe
+{
+    /// <summary>
+    /// Interaction logic for AddRecipe.xaml
+    /// </summary>
+    public partial class AddRecipe : Page
+    {
+        public AddRecipe()
+        {
+            InitializeComponent();  
+        }
+        BindingList<Recipe> _list;
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonBack_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonUpload_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonUploadStep_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonStepUpload_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void imgssstep_PreviewMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void AddimgStep_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddStep_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonCancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Food Recipe/Recipe.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Food_Recipe
+{
+    class Recipe:INotifyPropertyChanged
+    {
+        public string Title { get; set; }
+        public string Step { get; set; }
+        public string Picture { get; set; }
+        public string Description { get; set; }
+        public string Youtube { get; set; }
+        public string StepDescription { get; set; }
+        public BindingList<string> Imagesss { get; set; }
+        public string UIHeartColor { get; set; }
+        public string UIHeartIcon { get; set; }
+        public string WidthFood { get; set; }
+        public string Colorrrrrr { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+    }
+}
